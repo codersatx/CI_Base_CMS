1accecd82cd09dc1d1574791ed8a9faa2fc5d2f9
mpmont <mpmont@gmail.com>
2011-07-08 19:26:41 +0100

    Tradução
    Tradução de ficheiros de erro e confirmação / sistema.

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/admin_base/';
+$config['base_url']	= 'http://localhost:8888/CI_Base_CMS/';
 
 /*
 |--------------------------------------------------------------------------
